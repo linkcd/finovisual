c801ec7ccf25fb7536bfe952791e0c5139e8018b
linkcd <linkcd@gmail.com>
2015-12-04 21:11:43 +0100

    use google geo api

--- crawler/fino/pipelines.py
+++ crawler/fino/pipelines.py
@@ -7,23 +7,29 @@
 import requests
 import logging
 import pdb
+import os
 
 from scrapy.exceptions import DropItem
 from scrapy.utils.serialize import ScrapyJSONEncoder
 encoder = ScrapyJSONEncoder()
 
-from geopy.geocoders import Nominatim
+from geopy.geocoders import Nominatim, GoogleV3
 
 class AzureStorageItemPipeline(object):
 
     ws_url = "http://finovisualization.azurewebsites.net/api/RealEstates"
     headers = {'Content-Type': 'application/json'}
+    key = None
+    keyFileName = os.path.join(os.path.dirname(__file__), 'googleGeoAPI.key')
+
+    with open(keyFileName, "r") as keyFile:
+        key = keyFile.read().replace('\n', '')
 
     def process_item(self, item, spider):
         if item['askingPrice']:
 
             #geo code
-            geolocator = Nominatim()
+            geolocator = GoogleV3(api_key=self.key)
             location = geolocator.geocode(item["address"])
             if location:
                 item["latitude"] = location[1][0]
@@ -31,12 +37,10 @@ class AzureStorageItemPipeline(object):
             else:
                 pdb.set_trace()
 
-
             #http post
             data=encoder.encode(item)
-            #r = requests.post(self.ws_url, data, headers = self.headers)
+            r = requests.post(self.ws_url, data, headers = self.headers)
 
             return item
         else:
-            pdb.set_trace()
             raise DropItem("Missing price in %s" % item)
